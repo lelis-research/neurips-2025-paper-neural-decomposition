74d76ddfb79710ee57085ed235dd1d5e4822b4c7
unknown <behdin.p@gmail.com>
2025-07-04 21:18:56 -0600

    Modify train script

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=1G
-#SBATCH --time=01:20:00
+#SBATCH --time=00:40:00
 #SBATCH --output=MultiRoom/%A-%a.out
 #SBATCH --account=rrg-lelis
-#SBATCH --array=0-59
+#SBATCH --array=90-119
 
 source /home/iprnb/venvs/neural-policy-decomposition/bin/activate
 
@@ -16,13 +16,13 @@ export PYTHONPATH=":$PYTHONPATH"
 
 python3.11 ~/scratch/neurips-2025-paper-neural-decomposition/pipelines/train_ppo.py \
     --seed $SLURM_ARRAY_TASK_ID\
-    --env_id "MiniGrid-MultiRoom-v0"\
+    --env_id "MiniGrid-Unlock-v0"\
     --num_steps 2000\
     --game_width 9\
     --total_timesteps 1000000\
-    --save_run_info 1\
-    --method "options"\
-    --option_mode "neural-augmented"\
+    --save_run_info 0\
+    --method "no_options"\
+    --option_mode "vanilla"\
     --sweep_run 1
 
     # python3.11 ~/scratch/neurips-2025-paper-neural-decomposition/pipelines/train_ppo.py \
